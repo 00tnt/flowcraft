609006aac1508b88b0a295a2db3c144329d19c3a
ginn1111 <vanthuanjw@gmail.com>
2025-10-28 21:42:12 +0700

    chore: setup submodule

--- apps/be/src/main.ts
+++ apps/be/src/main.ts
@@ -1,1 +1,13 @@
-console.log('Hello, world!');
+import express from 'express';
+
+const app = express();
+
+app.get('/health', (_, res) => {
+  res.send('ok');
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});

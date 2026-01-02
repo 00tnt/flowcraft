d5a6362245cfd75c9fe3564676fe74635ea124dc
ginn1111 <vanthuanjw@gmail.com>
2026-01-02 15:46:40 +0700

    ci: ignore type check

--- apps/fe/next.config.ts
+++ apps/fe/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@repo/ui'],
+
+  outputFileTracingRoot: path.join(__dirname, '../../'),
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },

cc3289a06f74289549dce423084f92fa6b47b928
ginn1111 <vanthuanjw@gmail.com>
2026-01-02 17:02:47 +0700

    ci: update next.config.js

--- apps/fe/next.config.ts
+++ apps/fe/next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from 'next';
-import path from 'path';
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@repo/ui'],
-
-  outputFileTracingRoot: path.join(__dirname, '../../'),
 
   eslint: {
     ignoreDuringBuilds: true,

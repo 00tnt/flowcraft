e6f97573d648fe7e09acfed00add0d2941e9b416
thuanpvaioz <thuan.van.pham@aioz.io>
2026-01-09 18:09:54 +0700

    ci: update

--- apps/be/src/repositories/user/IUserRepository.ts
+++
@@ -1,8 +0,0 @@
-import { User } from '@/entities/user';
-
-export interface IUserRepository {
-  findById(id: string): Promise<User>;
-  findAll(): Promise<User[]>;
-  create(user: User): Promise<User>;
-  update(user: User): Promise<User>;
-}
--- init-submodules.sh
+++ init-submodules.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 echo "Updating all submodules..."
-git submodule update --init --recursive
+
+git rm -rf apps/be
+git submodule add -f -b develop https://github.com/flowcraft/flowcraft.git apps/be
 git submodule status
